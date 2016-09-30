a475be21c8f46b0a31c8cf8722fe49cc4dde8cda
Alisson Steffens Henrique <5663296@univali.br>
2016-09-29 21:48:51 -0300

    first commit

---
+++ src/codecad/parser/LexicalError.java
@@ -0,0 +1,14 @@
+package codecad.parser;
+
+public class LexicalError extends AnalysisError
+{
+    public LexicalError(String msg, int position)
+	 {
+        super(msg, position);
+    }
+
+    public LexicalError(String msg)
+    {
+        super(msg);
+    }
+}
---
+++ src/codecad/parser/Semantico.java
@@ -0,0 +1,9 @@
+package codecad.parser;
+
+public class Semantico implements Constants
+{
+    public void executeAction(int action, Token token)	throws SemanticError
+    {
+        System.out.println("A��o #"+action+", Token: "+token);
+    }	
+}
