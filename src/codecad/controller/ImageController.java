c56c0d9ac0484b98278508f0adfb98e53c9d5eec
Augus-top <acpluschkat@hotmail.com>
2016-10-04 16:39:13 -0300

    Iniciando Desenhos

--- src/codecad/controller/ImageController.java
+++ src/codecad/controller/ImageController.java
@@ -6,7 +6,14 @@
 package codecad.controller;
 
 import codecad.model.ImageVariable;
+import com.alee.extended.image.DisplayType;
+import com.alee.extended.image.WebImage;
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JPanel;
@@ -21,6 +28,11 @@ public class ImageController {
     private String caminhoNovaVariavel;
     private JPanel painelImagem;
     private List<ImageVariable> variaveis = new ArrayList<>();
+    private Stroke stroke = new BasicStroke(1);
+    private Color corLinha = Color.BLACK;
+    private Color corFill = Color.BLACK;
+    private Graphics2D g;
+    BufferedImage imagem;
     
     public ImageController(JPanel painelImagem) {
         this.painelImagem = painelImagem;
@@ -67,17 +79,17 @@ public class ImageController {
     }
     
     public void setStroke(){
-        
+        this.stroke = new BasicStroke(this.parametros.get(0));
         this.parametros.clear();
     }
     
     public void setColor(){
-        
+        this.corLinha = new Color(this.parametros.get(0), this.parametros.get(1), this.parametros.get(2));
         this.parametros.clear();
     }
     
     public void setFill(){
-        
+        this.corFill = new Color(this.parametros.get(0), this.parametros.get(1), this.parametros.get(2));
         this.parametros.clear();
     }
     
@@ -86,23 +98,47 @@ public class ImageController {
         this.parametros.clear();
     }
     
-    public void desenharPonto(){
-        
+    private void prepararDesenho(){
+        if(this.painelImagem.getComponentCount() == 0){
+            this.imagem = new BufferedImage(this.painelImagem.getWidth(), this.painelImagem.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+        }
+        this.painelImagem.removeAll();
+        this.g = (Graphics2D) this.imagem.getGraphics();
+        this.g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        this.g.setStroke(this.stroke);
+        this.g.setColor(this.corLinha);
+    }
+    
+    private void finalizarDesenho(){
+        WebImage wi = new WebImage(this.imagem);
+        wi.setDisplayType(DisplayType.fitComponent);
+        this.painelImagem.add(wi);
+        this.painelImagem.revalidate();
         this.parametros.clear();
+        this.painelImagem.revalidate();
+    }
+    
+    public void desenharPonto(){
+        this.prepararDesenho();
+        
+        this.finalizarDesenho();
     }
     
     public void desenharLinha(){
-        
-        this.parametros.clear();
+        this.prepararDesenho();
+        this.g.drawLine(this.parametros.get(0), this.parametros.get(1), this.parametros.get(2), this.parametros.get(3));
+        this.finalizarDesenho();
     }
     
     public void desenharRetangulo(){
+        this.prepararDesenho();
         
-        this.parametros.clear();
+        this.finalizarDesenho();
     }
     
     public void desenharElipse(){
+        this.prepararDesenho();
         
-        this.parametros.clear();
+        this.finalizarDesenho();
     }
 }
